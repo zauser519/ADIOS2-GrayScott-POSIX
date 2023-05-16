0d54a64182951636e87e1fcea9cf463e3ea73e49
zauser519 <56807402+zauser519@users.noreply.github.com>
2023-05-16 14:16:36 +0900

    Update writer.h

--- gray-scott/simulation/writer.h
+++ gray-scott/simulation/writer.h
@@ -11,9 +11,9 @@ class Writer
 {
 public:
     Writer(const Settings &settings, const GrayScott &sim);
-    void Wopen(const std::string &fname, bool append);
+    void Wopen(const std::string &fname);
     void Wwrite(int step, const GrayScott &sim);
-    void Wclose(int fd);
+    void Wclose();
 
 protected:
     int fd;
