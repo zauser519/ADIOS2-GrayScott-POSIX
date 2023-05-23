680303391d76c1b8a235dd5e1c1b66c61a97caec
zauser519 <56807402+zauser519@users.noreply.github.com>
2023-05-23 21:30:19 +0900

    Update writer.h

--- gray-scott/simulation/writer.h
+++ gray-scott/simulation/writer.h
@@ -12,11 +12,16 @@ class Writer
 public:
     Writer(const Settings &settings, const GrayScott &sim);
     void Wopen(const std::string &fname);
-    void Wwrite(int step, const GrayScott &sim, int total);
+    void Wwrite(int step, const GrayScott &sim, MPI_Comm comm, int rank);
     void Wclose();
 
 protected:
+    //write mode
     int fd;
+    //exscan, for each step
+    int perrank, writen_thisprocessor;
+    //allreduce, for steps
+    long long perstep=0, writen_thisstep;
     Settings settings;
 };
 
