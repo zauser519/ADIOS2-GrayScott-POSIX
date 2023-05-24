acc95dc53233ba5c1aa5fa4356166c6b24f971b7
zauser519 <56807402+zauser519@users.noreply.github.com>
2023-05-24 15:15:24 +0900

    Update writer.cpp

--- gray-scott/simulation/writer.cpp
+++ gray-scott/simulation/writer.cpp
@@ -15,7 +15,6 @@ Writer::Writer(const Settings &settings, const GrayScott &sim)
 void Writer::Wopen(const std::string &fname)
 {
     fd = open(fname.c_str(), O_CREAT | O_WRONLY, 0644);
-    //perstep = 0;
 }
 
 void Writer::Wwrite(int step, const GrayScott &sim, MPI_Comm comm, int rank)
@@ -31,31 +30,32 @@ void Writer::Wwrite(int step, const GrayScott &sim, MPI_Comm comm, int rank)
 
     //Write file into data.0
     //pointer
- 
-    //Initializer, perrank is the sum of the result of processors written so far in single time step
     perrank=0;
-    //Pointer before write, perrank gives how far the current time step written, perstep gives sum of written result before this step steps so far
-    lseek(fd, perrank + perstep*step/settings.plotgap, SEEK_SET);
-    //Write
+    writen_thisprocessor = u.size() * sizeof(double) + v.size() * sizeof(double) + sizeof(int);
+    writen_thisstep=0;
+    lseek(fd, perrank + perstep, SEEK_SET);
     write(fd, &step, sizeof(int));
     write(fd, u.data(), u.size() * sizeof(double));
     write(fd, v.data(), v.size() * sizeof(double));
-    //the size of one processor written
-    writen_thisprocessor = u.size() * sizeof(double) + v.size() * sizeof(double) + sizeof(int);
-    //get the sum of the result of processors written so far
     MPI_Exscan(&writen_thisprocessor, &perrank, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    //writen_thisstep is the sum of wriiten result this time step
-    MPI_Allreduce(&perrank, &writen_thisstep, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    std::cout << "perrank " << perrank
-      << " perstep " << perstep 
-      << " in rank " << rank  
-      << " u size " << u.size() * sizeof(double)
-      << " v size " << v.size() * sizeof(double)
+    MPI_Allreduce(&writen_thisprocessor, &writen_thisstep, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    std::cout << "In rank " << rank  
+      << " u size is " << u.size() * sizeof(double)
+      << " v size is " << v.size() * sizeof(double)
       << " step size " << sizeof(int)
-      << " Allreduce " << writen_thisstep
-      << " Total size" << writen_thisprocessor << std::endl;
-    //I still can't find how to make Allreduce produce the final position of a single step, so... it's hard coding
-    perstep += writen_thisstep*4/6;
+      << " Total size is " << writen_thisprocessor << std::endl;
+
+    std::cout << "Currently, in this time step, before processor " << rank
+      << " it wrote " << perrank << std::endl;
+    if (rank==0){
+        std::cout << "The sum of the size of previous time steps is " << perstep << std::endl;
+    }
+    std::cout << "The result of processor "<< rank
+      << " writes to " << perrank + perstep << std::endl;
+    if (rank==0){
+        std::cout << "This time step has size of " << writen_thisstep << std::endl;
+    }
+    perstep += writen_thisstep;
 
 
 }
