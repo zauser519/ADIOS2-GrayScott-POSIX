b1a7587864dcfb2c70b05512b3f2ed37afab97a2
zauser519 <56807402+zauser519@users.noreply.github.com>
2023-05-27 21:33:54 +0900

    Update writer.cpp

--- gray-scott/simulation/writer.cpp
+++ gray-scott/simulation/writer.cpp
@@ -43,33 +43,39 @@ void Writer::Wwrite(int step, const GrayScott &sim, MPI_Comm comm, int rank)
     //pointer
     perrank=0;
     writen_thisstep=0;
+    
+    writen_thisprocessor = u.size() * sizeof(double) + v.size() * sizeof(double);
+    MPI_Exscan(&writen_thisprocessor, &perrank, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
     //prevent write repeatly
     if (perrank==0){
       fast_write(&step, sizeof(int));
     }
-    
-    writen_thisprocessor = u.size() * sizeof(double) + v.size() * sizeof(double) + sizeof(int);
-    MPI_Exscan(&writen_thisprocessor, &perrank, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
-    writen_thisprocessor = u.size() * sizeof(double) + v.size() * sizeof(double) + sizeof(int);
-    lseek(fd, perrank+perstep, SEEK_SET);
+    lseek(fd, sizeof(int)*step+perrank+perstep, SEEK_SET);
     fast_write(u.data(), u.size() * sizeof(double));
     fast_write(v.data(), v.size() * sizeof(double));
     MPI_Allreduce(&writen_thisprocessor, &writen_thisstep, 1, MPI_UNSIGNED_LONG, MPI_SUM, MPI_COMM_WORLD);
+    if(perrank==0){
+        std::cout << "The sum of the size of previous time steps is " << perstep << std::endl;
     std::cout << "In rank " << rank  
       << " u size is " << u.size() * sizeof(double)
       << " v size is " << v.size() * sizeof(double)
-      << " step size " << sizeof(int)
+      << " step " << sizeof(int)
       << " Total size is " << writen_thisprocessor << std::endl;
-
     std::cout << "Currently, in this time step, before processor " << rank
       << " it wrote " << perrank << std::endl;
-    if (rank==0){
-        std::cout << "The sum of the size of previous time steps is " << perstep << std::endl;
-    }
     std::cout << "The result of processor "<< rank
       << " writes to " << perrank + perstep << std::endl;
-    if (rank==0){
         std::cout << "This time step has size of " << writen_thisstep << std::endl;
+    }else
+    {
+      std::cout << "In rank " << rank  
+        << " u size is " << u.size() * sizeof(double)
+        << " v size is " << v.size() * sizeof(double)
+        << " Total size is " << writen_thisprocessor << std::endl;
+    std::cout << "Currently, in this time step, before processor " << rank
+      << " it wrote " << perrank << std::endl;
+    std::cout << "The result of processor "<< rank
+      << " writes to " << perrank + perstep << std::endl;
     }
     perstep += writen_thisstep;
 
